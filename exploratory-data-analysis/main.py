c18831ea20ecad494e8a6a3924dcd51af37704de
rkeenan <ross.keenan@protonmail.comm>
2023-07-25 14:10:36 -0500

    Added automatic summary statistics.

--- exploratory-data-analysis/main.py
+++ exploratory-data-analysis/main.py
@@ -1,17 +1,22 @@
 import pandas as pd
 hourCsv = pd.read_csv('./hour.csv')
 
+print(hourCsv.head())
+
+# Summary statistics - manual way
 countMean = hourCsv['count'].mean()
 countMedian = hourCsv['count'].median()
 countStandardDeviation = hourCsv['count'].std()
 registeredMinimum = hourCsv['registered'].min()
 registeredMaximum = hourCsv['registered'].max()
 
-print(hourCsv.head())
-
-# Summary statistics
 print(f'Count mean: {countMean}')
 print(f'Count median: {countMedian}')
 print(f'Count standard deviation: {countStandardDeviation}')
 print(f'Registered Minimum: {registeredMinimum}')
 print(f'Registered Maximum: {registeredMaximum}')
+
+# Summary statistics - automatic way
+hourDescribe = hourCsv.describe()
+
+print(hourDescribe)
