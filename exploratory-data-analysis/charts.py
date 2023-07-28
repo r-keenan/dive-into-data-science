386d7d715aa2ff14ad59911c8ff013bbb028693b
rkeenan <ross.keenan@protonmail.comm>
2023-07-28 10:38:44 -0500

    Added comment for type of graph.

--- exploratory-data-analysis/charts.py
+++ exploratory-data-analysis/charts.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 hourCsv = pd.read_csv('./hour.csv')
 
+# scatter plot
 fix, ax = plt.subplots(figsize=(10, 6))
 ax.scatter(x=hourCsv['instant'], y=hourCsv['count'])
 plt.show()
